95386aba9d26af99344ae8225cf4b6017f2ba625
write4alive <cenk.adalan@gmail.com>
2021-08-29 17:06:36 +0300

    create_infrastructure

---
+++ create_infrastructure.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import boto3
+import json
+import configparser
+from botocore.exceptions import ClientError
+
+
+# Load DWH Params from a file
+config = configparser.ConfigParser()
+config.read_file(open('dwh.cfg'))
+
+KEY                    = config.get('AWS','KEY')
+SECRET                 = config.get('AWS','SECRET')
+
+DWH_CLUSTER_TYPE       = config.get("DWH","DWH_CLUSTER_TYPE")
+DWH_NUM_NODES          = config.get("DWH","DWH_NUM_NODES")
+DWH_NODE_TYPE          = config.get("DWH","DWH_NODE_TYPE")
+
+DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
+DWH_DB                 = config.get("DWH","DWH_DB")
+DWH_DB_USER            = config.get("DWH","DWH_DB_USER")
+DWH_DB_PASSWORD        = config.get("DWH","DWH_DB_PASSWORD")
+DWH_PORT               = config.get("DWH","DWH_PORT")
+
+DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
+
+(DWH_DB_USER, DWH_DB_PASSWORD, DWH_DB)
+
+pd.DataFrame({"Param":
+                  ["DWH_CLUSTER_TYPE", "DWH_NUM_NODES", "DWH_NODE_TYPE", "DWH_CLUSTER_IDENTIFIER", "DWH_DB", "DWH_DB_USER", "DWH_DB_PASSWORD", "DWH_PORT", "DWH_IAM_ROLE_NAME"],
+              "Value":
+                  [DWH_CLUSTER_TYPE, DWH_NUM_NODES, DWH_NODE_TYPE, DWH_CLUSTER_IDENTIFIER, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, DWH_PORT, DWH_IAM_ROLE_NAME]
+             })
+
+
+# Create clients for IAM, EC2, S3 and Redshift
+ec2 = boto3.resource('ec2',
+                       region_name="us-west-2",
+                       aws_access_key_id=config.get('AWS','KEY'),
+                       aws_secret_access_key=config.get('AWS','SECRET')
+                    )
+
+s3 = boto3.resource('s3',
+                       region_name="us-west-2",
+                       aws_access_key_id=config.get('AWS','KEY'),
+                       aws_secret_access_key=config.get('AWS','SECRET')
+                   )
+
+iam = boto3.client('iam',
+                     region_name='us-west-2',
+                     aws_access_key_id=config.get('AWS','KEY'),
+                     aws_secret_access_key=config.get('AWS','SECRET')
+                     
+                  )
+
+redshift = boto3.client('redshift',
+                       region_name="us-west-2",
+                       aws_access_key_id=config.get('AWS','KEY'),
+                       aws_secret_access_key=config.get('AWS','SECRET')
+                       )
+
+# Check out the sample data sources on S3
+sampleDbBucket =  s3.Bucket("awssampledbuswest2")
+for obj in sampleDbBucket.objects.filter(Prefix="ssbgz"):
+    print(obj)
+# for obj in sampleDbBucket.objects.all():
+    # print(obj)
+
+#1.1 Create the role, 
+try:
+    print("1.1 Creating a new IAM Role") 
+    dwhRole = iam.create_role(
+        Path='/',
+        RoleName=DWH_IAM_ROLE_NAME,
+        Description = "Allows Redshift clusters to call AWS services on your behalf.",
+        AssumeRolePolicyDocument=json.dumps(
+            {'Statement': [{'Action': 'sts:AssumeRole',
+               'Effect': 'Allow',
+               'Principal': {'Service': 'redshift.amazonaws.com'}}],
+             'Version': '2012-10-17'})
+    )    
+except Exception as e:
+    print(e)
+    
+    
+print("1.2 Attaching Policy")
+
+iam.attach_role_policy(RoleName=DWH_IAM_ROLE_NAME,
+                       PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+                      )['ResponseMetadata']['HTTPStatusCode']
+
+print("1.3 Get the IAM role ARN")
+roleArn = iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
+
+print(roleArn)
+
+
+
+# Redshift Cluster
+try:
+    response = redshift.create_cluster(        
+        #DHW
+        ClusterType=DWH_CLUSTER_TYPE,
+        NodeType=DWH_NODE_TYPE,
+        NumberOfNodes=int(DWH_NUM_NODES),
+
+        #Identifiers & Credentials
+        DBName=DWH_DB,
+        ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+        MasterUsername=DWH_DB_USER,
+        MasterUserPassword=DWH_DB_PASSWORD,
+        
+        #Roles (for s3 access)
+        IamRoles=[roleArn]  
+    )
+except Exception as e:
+    print(e)
+
+
+# Describe the cluster to see its status
+def prettyRedshiftProps(props):
+    pd.set_option('display.max_colwidth', -1)
+    keysToShow = ["ClusterIdentifier", "NodeType", "ClusterStatus", "MasterUsername", "DBName", "Endpoint", "NumberOfNodes", 'VpcId']
+    x = [(k, v) for k,v in props.items() if k in keysToShow]
+    return pd.DataFrame(data=x, columns=["Key", "Value"])
+
+myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
+prettyRedshiftProps(myClusterProps)
+
+
+
+# Open an incoming TCP port to access the cluster ednpoint
+try:
+    vpc = ec2.Vpc(id=myClusterProps['VpcId'])
+    defaultSg = list(vpc.security_groups.all())[0]
+    print(defaultSg)
+    defaultSg.authorize_ingress(
+        GroupName=defaultSg.group_name,
+        CidrIp='0.0.0.0/0',
+        IpProtocol='TCP',
+        FromPort=int(DWH_PORT),
+        ToPort=int(DWH_PORT)
+    )
+except Exception as e:
+    print(e)
+
+
+# Clean up your resources
+#### CAREFUL!!
+#-- Uncomment & run to delete the created resources
+# redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
+#### CAREFUL!!
+
+# myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
+# prettyRedshiftProps(myClusterProps)
+
+# #### CAREFUL!!
+# #-- Uncomment & run to delete the created resources
+# iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+# iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
+# #### CAREFUL!!
+
+
+
+
+
+
+
--- sql_queries.py
+++ sql_queries.py
@@ -72,3 +72,60 @@ drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songp
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+
+
+
+
+
+# -------------------------------------------------------------------
+# DROP TABLES - First we drop all fact and dim tables if they exists.
+user_table_drop = "drop table if exists  d_users"
+song_table_drop = "drop table  if exists d_songs"
+artist_table_drop = "drop table  if exists d_artists"
+time_table_drop = "drop table  if exists d_time"
+songplay_table_drop = "drop table if exists f_songplays"
+
+# CREATE TABLES  - Creating Fact and Dim Tables of project.
+
+songplay_table_create = ("""create table if not exists f_songplays 
+(songplay_id serial primary key, start_time bigint, user_id int, level varchar, song_id varchar, artist_id varchar , session_id int not null , location varchar, user_agent varchar)
+""")
+
+user_table_create =     ("""create table if not exists d_users 
+(user_id int primary key, first_name varchar, last_name varchar, gender char(1),level varchar)
+""")
+
+song_table_create =     ("""create table if not exists d_songs 
+(song_id varchar primary key, title varchar, artist_id varchar, year int, duration float)
+""")
+
+artist_table_create =   ("""create table if not exists d_artists 
+(artist_id varchar primary key, name varchar, location varchar, latitude float, longitude float)
+""") 
+
+time_table_create =     ("""create table if not exists d_time 
+(start_time bigint primary key , hour int , day int ,week int ,month int , year int , weekday int )
+""")
+
+# INSERT RECORDS - Insert queries of fact and dim tables. Aoiding data duplication with using " ON CONFLICT DO ".
+
+songplay_table_insert = ("""insert into f_songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+values(%s,%s,%s,%s,%s,%s,%s,%s)
+""")
+
+user_table_insert = ("""insert into d_users(user_id, first_name, last_name, gender, level)
+values(%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
+""")
+
+song_table_insert = ("""insert into d_songs(song_id, title, artist_id, year, duration)
+values(%s,%s,%s,%s,%s) ON CONFLICT (song_id) DO NOTHING;
+""")
+
+artist_table_insert = ("""insert into d_artists(artist_id, name, location, latitude, longitude)
+values(%s,%s,%s,%s,%s) ON CONFLICT (artist_id) DO NOTHING
+""")
+
+time_table_insert = ("""insert into d_time(start_time, hour, day, week, month, year, weekday)
+values(%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (start_time) DO NOTHING;
+""")
+# -------------------------------------------------------------------
