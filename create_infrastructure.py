578ddfa8711733ee570f5d8c5b57aa0491fce915
write4alive <cenk.adalan@gmail.com>
2021-08-31 16:01:30 +0300

    update_create_infrastructure_as_code

--- create_infrastructure.py
+++ create_infrastructure.py
@@ -120,7 +120,7 @@ except Exception as e:
 
 # Describe the cluster to see its status
 def prettyRedshiftProps(props):
-    pd.set_option('display.max_colwidth', -1)
+    pd.set_option('display.max_colwidth',None)
     keysToShow = ["ClusterIdentifier", "NodeType", "ClusterStatus", "MasterUsername", "DBName", "Endpoint", "NumberOfNodes", 'VpcId']
     x = [(k, v) for k,v in props.items() if k in keysToShow]
     return pd.DataFrame(data=x, columns=["Key", "Value"])
@@ -150,130 +150,17 @@ try:
 except Exception as e:
     print(e)
 
+# def clean_up_resources():
+#     #### CAREFUL!!
+#     #-- Uncomment & run to delete the created resources
+#     redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
 
-def clean_up_resources():
-    #### CAREFUL!!
-    #-- Uncomment & run to delete the created resources
-    redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
+#     #### CAREFUL!! 
 
-    #### CAREFUL!! 
-
-clean_up_resources()
+# clean_up_resources()
 
 
 # ------------------------------------------------------
-
-# import pandas as pd
-# import boto3
-# import json
-# import configparser
-# from botocore.exceptions import ClientError
-
-
-# # Load DWH Params from a file
-# config = configparser.ConfigParser()
-# config.read_file(open('dwh.cfg'))
-
-# KEY                    = config.get('AWS','KEY')
-# SECRET                 = config.get('AWS','SECRET')
-
-
-# iam = boto3.client('iam',
-#                      region_name='us-west-2',
-#                      aws_access_key_id=config.get('AWS','KEY'),
-#                      aws_secret_access_key=config.get('AWS','SECRET')
-                     
-#                   )
-
-# redshift = boto3.client('redshift',
-#                        region_name="us-west-2",
-#                        aws_access_key_id=config.get('AWS','KEY'),
-#                        aws_secret_access_key=config.get('AWS','SECRET')
-#                        )
-
-# #1.1 Create the role, 
-# try:
-#     print("1.1 Creating a new IAM Role") 
-#     dwhRole = iam.create_role(
-#         Path='/',
-#         RoleName=DWH_IAM_ROLE_NAME,
-#         Description = "Allows Redshift clusters to call AWS services on your behalf.",
-#         AssumeRolePolicyDocument=json.dumps(
-#             {'Statement': [{'Action': 'sts:AssumeRole',
-#                'Effect': 'Allow',
-#                'Principal': {'Service': 'redshift.amazonaws.com'}}],
-#              'Version': '2012-10-17'})
-#     )    
-# except Exception as e:
-#     print(e)
-    
-    
-# print("1.2 Attaching Policy")
-
-# iam.attach_role_policy(RoleName=DWH_IAM_ROLE_NAME,
-#                        PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-#                       )['ResponseMetadata']['HTTPStatusCode']
-
-# print("1.3 Get the IAM role ARN")
-# roleArn = iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
-
-# print(roleArn)
-
-
-
-# # Redshift Cluster
-# try:
-#     response = redshift.create_cluster(        
-#         #DHW
-#         ClusterType=DWH_CLUSTER_TYPE,
-#         NodeType=DWH_NODE_TYPE,
-#         NumberOfNodes=int(DWH_NUM_NODES),
-
-#         #Identifiers & Credentials
-#         DBName=DWH_DB,
-#         ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
-#         MasterUsername=DWH_DB_USER,
-#         MasterUserPassword=DWH_DB_PASSWORD,
-        
-#         #Roles (for s3 access)
-#         IamRoles=[roleArn]  
-#     )
-# except Exception as e:
-#     print(e)
-
-
-# # Describe the cluster to see its status
-# def prettyRedshiftProps(props):
-#     pd.set_option('display.max_colwidth', -1)
-#     keysToShow = ["ClusterIdentifier", "NodeType", "ClusterStatus", "MasterUsername", "DBName", "Endpoint", "NumberOfNodes", 'VpcId']
-#     x = [(k, v) for k,v in props.items() if k in keysToShow]
-#     return pd.DataFrame(data=x, columns=["Key", "Value"])
-
-# myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
-# prettyRedshiftProps(myClusterProps)
-
-# # Take note of the cluster endpoint and role ARN 
-# DWH_ENDPOINT = myClusterProps['Endpoint']['Address']
-# DWH_ROLE_ARN = myClusterProps['IamRoles'][0]['IamRoleArn']
-# print("DWH_ENDPOINT :: ", DWH_ENDPOINT)
-# print("DWH_ROLE_ARN :: ", DWH_ROLE_ARN)
-
-
-
-# # Open an incoming TCP port to access the cluster ednpoint
-# try:
-#     vpc = ec2.Vpc(id=myClusterProps['VpcId'])
-#     defaultSg = list(vpc.security_groups.all())[0]
-#     print(defaultSg)
-#     defaultSg.authorize_ingress(
-#         GroupName=defaultSg.group_name,
-#         CidrIp='0.0.0.0/0',
-#         IpProtocol='TCP',
-#         FromPort=int(DWH_PORT),
-#         ToPort=int(DWH_PORT)
-#     )
-# except Exception as e:
-#     print(e)
 
 
 # # Clean up your resources
